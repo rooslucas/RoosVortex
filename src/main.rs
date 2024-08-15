d575e9a07193e21a6b9d213f85004da3d5f3b838
rooslucas <rooslucas@live.nl>
2024-08-15 16:42:50 +0200

    Create working stack

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ enum OpCode {
     MUL,
     DIV,
     POP,
-    PUSH,
+    PUSH(i32),
 }
 
 // Make a program list
@@ -14,49 +14,125 @@ enum OpCode {
 
 // make pointers
 fn main() {
-    let stack: Vec<i32> = vec![0, 100]; // make it a vector of all 0's
-    let program: Vec<OpCode> = vec![];
-    let stackpointer: i32 = 0;
+    let mut stack: Vec<i32> = vec![0; 100]; // make it a vector of all 0's
+    let programs: Vec<OpCode> = vec![
+        OpCode::PUSH(5),
+        OpCode::PUSH(4),
+        OpCode::ADD,
+        OpCode::PUSH(8),
+        OpCode::SUB,
+        OpCode::PUSH(37),
+        OpCode::PUSH(67),
+        OpCode::MUL,
+    ];
+    let mut stackpointer: usize = 0;
+
+    for program in programs {
+        match program {
+            OpCode::ADD => {
+                let rt = add(stack, stackpointer);
+                stack = rt.0;
+                stackpointer = rt.1;
+            }
+            OpCode::SUB => {
+                let rt = sub(stack, stackpointer);
+                stack = rt.0;
+                stackpointer = rt.1;
+            }
+            OpCode::MUL => {
+                let rt = mul(stack, stackpointer);
+                stack = rt.0;
+                stackpointer = rt.1;
+            }
+            OpCode::DIV => {
+                let rt = div(stack, stackpointer);
+                stack = rt.0;
+                stackpointer = rt.1;
+            }
+            OpCode::POP => {
+                let rt = pop(stack, stackpointer);
+                stack = rt.0;
+                stackpointer = rt.2;
+            } //pop(stack, stackpointer),
+            OpCode::PUSH(value) => {
+                let rt = push(value, stack, stackpointer);
+                stack = rt.0;
+                stackpointer = rt.1;
+            } //push(value, stack, stackpointer),
+        }
+        println!("{:?}", stack);
+    }
+
+    println!("{:?}", stack);
 }
 
 // define a match statement that makes the reactions for each opcode
 
-fn match_program(program: OpCode, stack: Vec<i32>, stackpointer: usize) {
-    match program {
-        OpCode::ADD => add(stack, stackpointer),
-        OpCode::SUB => sub(stack, stackpointer),
-        OpCode::MUL => mul(stack, stackpointer),
-        OpCode::DIV => div(stack, stackpointer),
-        OpCode::POP => println!("maybe later"), //pop(stack, stackpointer),
-        OpCode::PUSH => println!("maybe later"), //push(value, stack, stackpointer),
-    }
+fn add(mut stack: Vec<i32>, mut stackpointer: usize) -> (Vec<i32>, usize) {
+    let rt_a = pop(stack, stackpointer);
+    let a = rt_a.1;
+    stackpointer = rt_a.2;
+
+    let rt_b = pop(rt_a.0, stackpointer);
+    let b: i32 = rt_b.1;
+    stackpointer = rt_b.2;
+
+    let rt = push(a + b, rt_b.0, stackpointer);
+    stack = rt.0;
+    stackpointer = rt.1;
+
+    return (stack, stackpointer);
 }
 
-fn add(stack: Vec<i32>, stackpointer: usize) {
-    let a = pop(stack.clone(), stackpointer);
-    let b: i32 = pop(stack.clone(), stackpointer);
-    push(a + b, stack.clone(), stackpointer);
+fn sub(mut stack: Vec<i32>, mut stackpointer: usize) -> (Vec<i32>, usize) {
+    let rt_a = pop(stack, stackpointer);
+    let a = rt_a.1;
+    stackpointer = rt_a.2;
+
+    let rt_b = pop(rt_a.0, stackpointer);
+    let b: i32 = rt_b.1;
+    stackpointer = rt_b.2;
+
+    let rt = push(a - b, rt_b.0, stackpointer);
+    stack = rt.0;
+    stackpointer = rt.1;
+
+    return (stack, stackpointer);
 }
 
-fn sub(stack: Vec<i32>, stackpointer: usize) {
-    let a = pop(stack.clone(), stackpointer);
-    let b: i32 = pop(stack.clone(), stackpointer);
-    push(a - b, stack.clone(), stackpointer);
+fn mul(mut stack: Vec<i32>, mut stackpointer: usize) -> (Vec<i32>, usize) {
+    let rt_a = pop(stack, stackpointer);
+    let a = rt_a.1;
+    stackpointer = rt_a.2;
+
+    let rt_b = pop(rt_a.0, stackpointer);
+    let b: i32 = rt_b.1;
+    stackpointer = rt_b.2;
+
+    let rt = push(a * b, rt_b.0, stackpointer);
+    stack = rt.0;
+    stackpointer = rt.1;
+
+    return (stack, stackpointer);
 }
 
-fn mul(stack: Vec<i32>, stackpointer: usize) {
-    let a = pop(stack.clone(), stackpointer);
-    let b: i32 = pop(stack.clone(), stackpointer);
-    push(a * b, stack.clone(), stackpointer)
+fn div(mut stack: Vec<i32>, mut stackpointer: usize) -> (Vec<i32>, usize) {
+    let rt_a = pop(stack, stackpointer);
+    let a = rt_a.1;
+    stackpointer = rt_a.2;
+
+    let rt_b = pop(rt_a.0, stackpointer);
+    let b: i32 = rt_b.1;
+    stackpointer = rt_b.2;
+
+    let rt = push(a / b, rt_b.0, stackpointer);
+    stack = rt.0;
+    stackpointer = rt.1;
+
+    return (stack, stackpointer);
 }
 
-fn div(stack: Vec<i32>, stackpointer: usize) {
-    let a = pop(stack.clone(), stackpointer);
-    let b: i32 = pop(stack.clone(), stackpointer);
-    push(a / b, stack.clone(), stackpointer)
-}
-
-fn pop(mut stack: Vec<i32>, mut stackpointer: usize) -> i32 {
+fn pop(mut stack: Vec<i32>, mut stackpointer: usize) -> (Vec<i32>, i32, usize) {
     if stackpointer > 0 {
         stackpointer -= 1;
     } else {
@@ -65,10 +141,11 @@ fn pop(mut stack: Vec<i32>, mut stackpointer: usize) -> i32 {
 
     let rt: i32 = stack[stackpointer];
     stack[stackpointer] = 0;
-    return rt;
+    return (stack, rt, stackpointer);
 }
 
-fn push(value: i32, mut stack: Vec<i32>, mut stackpointer: usize) {
+fn push(value: i32, mut stack: Vec<i32>, mut stackpointer: usize) -> (Vec<i32>, usize) {
     stack[stackpointer] = value;
     stackpointer += 1;
+    return (stack, stackpointer);
 }
