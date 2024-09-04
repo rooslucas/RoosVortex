641f816889aed1020342a333606d3f3074506cfa
rooslucas <rooslucas@live.nl>
2024-09-04 20:52:17 +0200

    Start implementing functions

--- src/parser.rs
+++ src/parser.rs
@@ -14,18 +14,53 @@ enum Keywords {
 
 fn parse() {
     //TODO: parse input to good format
+    let mut function_list: Vec<String> = vec![];
 
     match p {
-        Keywords::Air => todo!(),
-        Keywords::Updraft(value1, value2) => todo!(),
-        Keywords::Suction(value1, value2) => todo!(),
-        Keywords::Twister(value1, value2) => todo!(),
-        Keywords::Churn(value1, value2) => todo!(),
+        Keywords::Air => function_list.push(todo!()),
+
+        Keywords::Updraft(value1, value2) => {
+            todo!(); // find value1 on stack
+            todo!(); // find value2 on stack
+            function_list.push("ADD".to_owned());
+        }
+
+        Keywords::Suction(value1, value2) => {
+            todo!(); // find value1 on stack
+            todo!(); // find value2 on stack
+            function_list.push("SUB".to_owned());
+        }
+
+        Keywords::Twister(value1, value2) => {
+            todo!(); // find value1 on stack
+            todo!(); // find value2 on stack
+            function_list.push("MUL".to_owned());
+        }
+
+        Keywords::Churn(value1, value2) => {
+            todo!(); // find value1 on stack
+            todo!(); // find value2 on stack
+            function_list.push("DIV".to_owned());
+        }
+
         Keywords::Storm => todo!(),
+
         Keywords::Gyre => todo!(),
-        Keywords::Whirl(statement) => todo!(),
-        Keywords::Swirl => todo!(),
+
+        Keywords::Whirl(statement) => {
+            todo!(); // find value1 on stack
+            todo!(); // find value2 on stack
+            function_list.push("IF ".to_owned() + &statement.to_string());
+        }
+
+        Keywords::Swirl => {
+            todo!(); // find value1 on stack
+            todo!(); // find value2 on stack
+            function_list.push("ELSE".to_owned());
+        }
+
         Keywords::Typhoon => todo!(),
+
         Keywords::Funnel => todo!(),
     }
 }
