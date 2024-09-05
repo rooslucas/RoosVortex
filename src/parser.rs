81a5c80944fd99ab97f40ef63e2e371021b2ea38
rooslucas <rooslucas@live.nl>
2024-09-05 15:28:56 +0200

    Implement basic parser

--- src/parser.rs
+++ src/parser.rs
@@ -20,7 +20,10 @@ impl FromStr for Keywords {
     type Err = ();
 
     fn from_str(input: &str) -> Result<Keywords, Self::Err> {
-        let function_line: Vec<&str> = input.split_whitespace().collect();
+        let new_input = input.replace(&['(', ')', ','][..], " ");
+
+        let function_line: Vec<&str> = new_input.split_whitespace().collect();
+        let function = function_line[0];
         let mut value1 = 0;
         let mut value2 = 0;
         let mut variable_name: String = "".to_owned();
@@ -28,7 +31,7 @@ impl FromStr for Keywords {
         if (function_line.len() > 1) & (function_line[0] == "Storm") {
             variable_name = function_line[1].to_owned();
             value1 = function_line[2].parse::<i32>().unwrap();
-        } else if (function_line.len() > 1) & (function_line[0] == "Updraf") {
+        } else if (function_line.len() > 1) & (function_line[0] == "Updraft") {
             value1 = function_line[1].parse::<i32>().unwrap();
             value2 = function_line[2].parse::<i32>().unwrap();
         }
@@ -38,74 +41,79 @@ impl FromStr for Keywords {
         // if storm variable name = .0.1
         // find air value after is value
 
-        match &function {
-            "Storm" => Ok(Keywords::Storm(todo!(), todo!())),
-            "Updraft" => Ok(Keywords::Updraft(todo!(), todo!())),
-            "Suction" => Ok(Keywords::Suction(todo!(), todo!())),
-            "Twister" => Ok(Keywords::Twister(todo!(), todo!())),
-            "Churn" => Ok(Keywords::Churn(todo!(), todo!())),
-            i32 => return I32(&function.parse::<i32>().unwrap()),
+        match function {
+            "Storm" => Ok(Keywords::Storm(variable_name, value1)),
+            "Updraft" => Ok(Keywords::Updraft(value1, value2)),
+            "Suction" => Ok(Keywords::Suction(value1, value2)),
+            "Twister" => Ok(Keywords::Twister(value1, value2)),
+            "Churn" => Ok(Keywords::Churn(value1, value2)),
+            function_line if function_line.parse::<i32>().is_ok() => {
+                Ok(Keywords::I32(function_line.clone().parse::<i32>().unwrap()))
+            }
             _ => Err(()),
         }
     }
 }
 
-fn parse() {
+fn parse(program_text: Vec<String>) {
     //TODO: parse input to good format
     let mut function_list: Vec<String> = vec![];
     let mut heap = HashMap::new();
+    for program_line in program_text {
+        let p = Keywords::from_str(&program_line).unwrap();
 
-    match p {
-        Keywords::Air => {
-            todo!()
+        match p {
+            Keywords::Air => {
+                todo!()
+            }
+
+            Keywords::Updraft(value1, value2) => {
+                todo!(); // find value1 on stack
+                todo!(); // find value2 on stack
+                function_list.push("ADD".to_owned());
+            }
+
+            Keywords::Suction(value1, value2) => {
+                todo!(); // find value1 on stack
+                todo!(); // find value2 on stack
+                function_list.push("SUB".to_owned());
+            }
+
+            Keywords::Twister(value1, value2) => {
+                todo!(); // find value1 on stack
+                todo!(); // find value2 on stack
+                function_list.push("MUL".to_owned());
+            }
+
+            Keywords::Churn(value1, value2) => {
+                todo!(); // find value1 on stack
+                todo!(); // find value2 on stack
+                function_list.push("DIV".to_owned());
+            }
+
+            Keywords::Storm(variable, value) => {
+                heap.insert(variable, value);
+            }
+
+            Keywords::Gyre => todo!(),
+
+            Keywords::Whirl(statement) => {
+                todo!(); // find value1 on stack
+                todo!(); // find value2 on stack
+                function_list.push("IF ".to_owned() + &statement.to_string());
+            }
+
+            Keywords::Swirl => {
+                todo!(); // find value1 on stack
+                todo!(); // find value2 on stack
+                function_list.push("ELSE".to_owned());
+            }
+
+            Keywords::Typhoon => todo!(),
+
+            Keywords::Funnel(value) => todo!(),
+
+            Keywords::I32(value) => todo!(),
         }
-
-        Keywords::Updraft(value1, value2) => {
-            todo!(); // find value1 on stack
-            todo!(); // find value2 on stack
-            function_list.push("ADD".to_owned());
-        }
-
-        Keywords::Suction(value1, value2) => {
-            todo!(); // find value1 on stack
-            todo!(); // find value2 on stack
-            function_list.push("SUB".to_owned());
-        }
-
-        Keywords::Twister(value1, value2) => {
-            todo!(); // find value1 on stack
-            todo!(); // find value2 on stack
-            function_list.push("MUL".to_owned());
-        }
-
-        Keywords::Churn(value1, value2) => {
-            todo!(); // find value1 on stack
-            todo!(); // find value2 on stack
-            function_list.push("DIV".to_owned());
-        }
-
-        Keywords::Storm(variable, value) => {
-            heap.insert(variable, value);
-        }
-
-        Keywords::Gyre => todo!(),
-
-        Keywords::Whirl(statement) => {
-            todo!(); // find value1 on stack
-            todo!(); // find value2 on stack
-            function_list.push("IF ".to_owned() + &statement.to_string());
-        }
-
-        Keywords::Swirl => {
-            todo!(); // find value1 on stack
-            todo!(); // find value2 on stack
-            function_list.push("ELSE".to_owned());
-        }
-
-        Keywords::Typhoon => todo!(),
-
-        Keywords::Funnel(value) => todo!(),
-
-        Keywords::I32(value) => todo!(),
     }
 }
